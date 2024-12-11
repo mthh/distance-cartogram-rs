2704f75bb014b3f6ecd5744acc57b7172487e0b8
Matthieu Viry <matthieu.viry@cnrs.fr>
2024-12-11 15:00:17 +0100

    Remove useless helper function

--- src/utils.rs
+++ src/utils.rs
@@ -1,14 +1,4 @@
-use crate::bbox::BBox;
-use crate::rectangle::Rectangle2D;
 use geo_types::Coord;
-
-pub(crate) fn get_bbox(points: &[Coord]) -> BBox {
-    let mut rect = Rectangle2D::new(0.0, 0.0, 0.0, 0.0);
-    for pt in points {
-        rect.add(pt);
-    }
-    rect.as_bbox()
-}
 
 pub(crate) fn distance_sq(p1: &Coord, p2: &Coord) -> f64 {
     (p1.x - p2.x).powi(2) + (p1.y - p2.y).powi(2)
