a161f411c9fa703d702bc3b9505c340d9f969029
Matthieu Viry <matthieu.viry@cnrs.fr>
2025-01-13 16:14:12 +0100

    Slightly simplify stuff in adjustment

--- src/adjustment.rs
+++ src/adjustment.rs
@@ -49,14 +49,7 @@ pub fn adjust(
     img_mean_y /= n as f64;
 
     // Compute adjustment coefficients
-    let mut a11 = 0.0;
-    let mut a12 = 0.0;
-    let mut a13 = 0.0;
-    let mut a21 = 0.0;
-    let mut a22 = 0.0;
-    let mut a23 = 0.0;
-
-    match adjustment_type {
+    let (a11, a12, a13, a21, a22, a23) = match adjustment_type {
         AdjustmentType::Euclidean => {
             let mut num1 = 0.0;
             let mut num2 = 0.0;
@@ -67,16 +60,16 @@ pub fn adjust(
                     + (src.1 - src_mean_y) * (img.1 - img_mean_y);
                 num2 += (src.0 - src_mean_x) * (img.1 - img_mean_y)
                     - (src.1 - src_mean_y) * (img.0 - img_mean_x);
-                denom += (img.0 - img_mean_x).powi(2)
-                    + (img.1 - img_mean_y).powi(2);
+                denom += (img.0 - img_mean_x).powi(2) + (img.1 - img_mean_y).powi(2);
             }
 
-            a11 = num1 / denom;
-            a12 = num2 / denom;
-            a21 = -a12;
-            a22 = a11;
-            a13 = src_mean_x - a11 * img_mean_x - a12 * img_mean_y;
-            a23 = src_mean_y - a21 * img_mean_x - a22 * img_mean_y;
+            let a11 = num1 / denom;
+            let a12 = num2 / denom;
+            let a21 = -a12;
+            let a22 = a11;
+            let a13 = src_mean_x - a11 * img_mean_x - a12 * img_mean_y;
+            let a23 = src_mean_y - a21 * img_mean_x - a22 * img_mean_y;
+            (a11, a12, a13, a21, a22, a23)
         }
         AdjustmentType::Affine => {
             let mut u2 = 0.0;
@@ -102,14 +95,15 @@ pub fn adjust(
             }
 
             let denom = uv.powi(2) - u2 * v2;
-            a11 = (uv * xv - v2 * xu) / denom;
-            a12 = (uv * xu - u2 * xv) / denom;
-            a21 = (uv * yv - v2 * yu) / denom;
-            a22 = (uv * yu - u2 * yv) / denom;
-            a13 = src_mean_x - a11 * img_mean_x - a12 * img_mean_y;
-            a23 = src_mean_y - a21 * img_mean_x - a22 * img_mean_y;
+            let a11 = (uv * xv - v2 * xu) / denom;
+            let a12 = (uv * xu - u2 * xv) / denom;
+            let a21 = (uv * yv - v2 * yu) / denom;
+            let a22 = (uv * yu - u2 * yv) / denom;
+            let a13 = src_mean_x - a11 * img_mean_x - a12 * img_mean_y;
+            let a23 = src_mean_y - a21 * img_mean_x - a22 * img_mean_y;
+            (a11, a12, a13, a21, a22, a23)
         }
-    }
+    };
 
     // Compute adjusted points
     let adjusted_points = image_points
